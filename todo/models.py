9a6b63533fcc4d268f1da3c5a9e46de0af4e1c4a
puzzle59 <cimettamarvin@gmail.com>
2020-07-22 19:56:30 +0200

    Astuce bancale car pas bcq donnèes effectuée, je vais voir si en réinsérant simplement le model Todo ça colle ou non

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 # modele todo
-
-class Todo(models.Model):
-    title = models.CharField(max_length= 100)
-    memo = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add= True)
-    datecompleted = models.DateTimeField(null=True)
-    important= models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
