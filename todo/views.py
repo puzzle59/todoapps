417aca0319afab05bfd0bedd0931ef3e3e2fa783
puzzle59 <cimettamarvin@gmail.com>
2020-07-22 16:43:01 +0200

    test interface

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login , logout , authenticate
+from .models import Todo
 
 def home(request):
     return render(request, 'todo/home.html')
