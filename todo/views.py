36e27b1a6cbd0c7d3f0c9419e9eebcdd98aa439e
puzzle59 <cimettamarvin@gmail.com>
2020-07-23 01:59:29 +0200

    Donegit add .! Styling will be handled later

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect ,  get_object_or_404
 from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login , logout , authenticate
 from .forms import TodoForm
-
+from .models import Todo
+from django.utils import timezone
+#security
+from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'todo/home.html')
 def signupuser(request):
@@ -37,14 +40,14 @@ def loginuser(request):
             login(request, user)
             return redirect('currenttodos')
 
-
+@login_required
 def logoutuser(request):
     if request.method=='POST':
         logout(request)
         return redirect('home')
     else:
         return redirect('currenttodos')
-
+@login_required
 def createtodo(request):
     if request.method =='GET':
         return render(request, 'todo/createtodo.html' , {'form':TodoForm()})
@@ -56,7 +59,50 @@ def createtodo(request):
             newtodo.save()
             return redirect('currenttodos')
         except ValueError:
-            return render(request, 'todo/createtodo.html' , {'form':TodoForm(),'error':'Bad data passed in. Please try again'})    
+            return render(request, 'todo/createtodo.html' , {'form':TodoForm(),'error':'Bad data passed in. Please try again'})
 
+
+@login_required
 def currenttodos(request):
-    return render(request,'todo/currenttodos.html')
+    todos = Todo.objects.filter(user = request.user , datecompleted__isnull=True)
+    return render(request,'todo/currenttodos.html',{'todos':todos})
+@login_required
+def viewtodo(request , todo_pk):
+#user for secyurity
+    todo= get_object_or_404(Todo,pk = todo_pk , user=request.user)
+    if request.method == 'GET':
+
+        #tout simple mais important la ligne qui suit : affiche le formulaire en fait
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    else:
+        try:
+            #Attention à prendre en compte le user , donc la todo courante,
+            #défini par instance ...
+            form = TodoForm(request.POST , instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo':todo, 'form': form,'error':'Bad data'})
+@login_required
+def completetodo(request,todo_pk):
+    # No template , on purpose
+    todo = get_object_or_404(Todo,pk=todo_pk,user= request.user )
+    if request.method =='POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+@login_required
+def deletetodo(request,todo_pk):
+    # No template , on purpose
+    todo = get_object_or_404(Todo,pk=todo_pk,user= request.user )
+    if request.method =='POST':
+
+        todo.delete()
+        return redirect('currenttodos')
+
+@login_required
+def completedtodos(request):
+        todos = Todo.objects.filter(user = request.user , datecompleted__isnull=False).order_by('-datecompleted')
+        return render(request,'todo/completedtodos.html',{'todos':todos})
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -25,7 +25,12 @@ path('login/',views.loginuser,name='loginuser'),
 path('logout/',views.logoutuser,name='logoutuser'),
 
     #To dos
+path('',views.home,name='home'),
 path('create/',views.createtodo,name='createtodo'),
 path('current/',views.currenttodos,name='currenttodos'),
-path('',views.home,name='home'),
+path('completed/',views.completedtodos,name='completedtodos'),
+path('todo/<int:todo_pk>',views.viewtodo,name='viewtodo'),
+path('todo/<int:todo_pk>/complete',views.completetodo,name='completetodo'),
+path('todo/<int:todo_pk>/delete',views.deletetodo,name='deletetodo'),
+#entre crochet  c'est un entier
 ]
