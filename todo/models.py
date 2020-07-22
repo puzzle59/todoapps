7c044c8684735b39387b8d002c4ae4bdfd7bf987
puzzle59 <cimettamarvin@gmail.com>
2020-07-22 17:13:09 +0200

    QCM Ok , rajout choix par def

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,14 @@ class Todo(models.Model):
     title=models.CharField(max_length=200)
     description=models.TextField()
 
-
+    #test pour le choix multiple:
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES , default='S')
+#issue documentation
 
 
     def __str__(self):
