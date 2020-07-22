1aa1bbe8def07b9ae51bff7a6156c15738f52edf
puzzle59 <cimettamarvin@gmail.com>
2020-07-22 18:27:36 +0200

    migration du modèle todo fonctionnel, ça avance

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # modele todo
 class Todo(models.Model):
-    title=models.CharField(max_length=200)
-    description=models.TextField()
+    title=models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    #date exact creation
+    datecompleted= models.DateTimeField(null=True)
 
+    #boolean for important ,or not choice
+    important= models.BooleanField(default=False)
+
+    #user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     #test pour le choix multiple:
     SHIRT_SIZES = (
         ('S', 'Small'),
